0d9cb61293ddedf5472019837aa88df2898eb427
ngoc <ngo.khanh.ngoc@gmail.com>
2019-11-03 14:59:40 -0800

    Set <p>'s containing answers in FAQ section to be visible but still be in document's flow with heights for JS to obtain them to animate opening/closing effect

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+/***********************************************
+Global variables
+***********************************************/
+
+var h = []; //array for <p> elements' heights in FAQ section
+/***********************************************
+Preload script
+***********************************************/
+
 window.addEventListener('load', function() {
     document.querySelector('body').classList.remove('preload');
+
+    // Getting heights of <p> elements in FAQ section, needed for transition animation
+    // which won't work with auto or initial height
+    var questionCount = document.querySelector(".faq-questions").childElementCount;
+    console.log(questionCount);
+    for (var i = 0; i < questionCount; i++) {
+        var answer = document.querySelector('.faq-answer' + (i + 1));
+        h[i] = answer.clientHeight;
+
+    return h
+})
+
 })
