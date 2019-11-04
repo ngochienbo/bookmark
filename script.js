c3610b36d7367e42ff0b668b584eb1ce9c3d09a7
ngoc <ngo.khanh.ngoc@gmail.com>
2019-11-03 20:57:39 -0800

    Remove leftover testing object

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ window.addEventListener('load', function() {
     // Getting heights of <p> elements in FAQ section, needed for transition animation
     // which won't work with auto or initial height
     var questionCount = document.querySelector(".faq-questions").childElementCount;
-    console.log(questionCount);
     for (var i = 0; i < questionCount; i++) {
         var answer = document.querySelector('.faq-answer' + (i + 1));
         h[i] = answer.clientHeight;
